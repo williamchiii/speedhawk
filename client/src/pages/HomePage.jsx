87d1537a37826beba78370b1cf9949d2a74feeec
William Chi <williamc28275311@yahoo.com>
2026-03-30 23:52:05 -0400

    improved homepage UI

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import TestAudit from "../components/TestAudit.jsx";
+import Navbar from "../components/Navbar.jsx";
 
 const HomePage = () => {
   return (
     <div>
-        <h1>HomePage</h1>
-        <a href="http://localhost:5173/health">Check Client/Server Connection Health</a>
+      <Navbar />
+      <div className="divider -mt-1"/>
+      <div className="min-h-screen flex flex-col items-center gap-8 pt-16 px-4">
+        <div className="text-7xl">Analyze a Website</div>
         <TestAudit/>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomePage
