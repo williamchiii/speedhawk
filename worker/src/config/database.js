cf28dd4c2b6d537561debcf5907da7f1ea6bdaf8
William Chi <williamc28275311@yahoo.com>
2026-04-30 23:42:27 -0400

    created shared cert location for AWS RDS, and updated docker-compose-yml (#11)

--- worker/src/config/database.js
+++ worker/src/config/database.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Adjust the relative path based on where your cert actually lives
-const certPath = path.resolve(__dirname, "../../certs/global-bundle.pem");
+const certPath = path.resolve(__dirname, "../../../shared/certs/global-bundle.pem");
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
